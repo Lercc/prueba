1e030fac9d36f15d04baf73acb60f88dcc5236a4
lercc <lercc.en@gmail.com>
2020-11-15 01:43:18 -0500

    datos persistente localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
   <div id="app">
-    <notifications></notifications>
-    <router-view/>
+    <notifications v-show="!componentLoading"></notifications>
+    <router-view v-show="!componentLoading"/>
+    <div class="app-loader-content " v-show="componentLoading">
+      <hash-loader class="loader" :loading="componentLoading" :size = "120"  :color="'#2B2D64'"/>
+    </div>
   </div>
 </template>
 
 <script>
 import * as auth from '@/api/auth';
-import { mapMutations } from 'vuex';
+import { mapMutations, mapState } from 'vuex';
 export default {
 
-beforeCreate () {
-  auth.getInitialToken()
-    .then( res => {
-      this.setAppToken(res.data.access_token)
-    })
-    .catch( error => {
-      console.log(error)
-    })
-    .finally( () => {
-      console.log("get token inicial end")
-    })
+  data() {
+    return {
+      componentLoading: false,
+    }
   },
 
-  methods: {
-    ...mapMutations('appToken', [ "setAppToken" ] ),
-    ...mapMutations('estudianteToken', [ "setEstudianteToken" ] ),
-    ...mapMutations('usuario', [ "userIsAdmin" ] )
-  }
+  computed: {
+    ...mapState('usuario',["admin","token"])
+  },
+
+  beforeCreate () {
+    this.componentLoading = true
+    auth.getInitialToken()
+      .then( res => {
+        this.setAppToken(res.data.access_token)
+        this.cargarLocalStorageData()
+      })
+      .catch( error => {
+        console.log(error)
+      })
+      .finally( () => {
+        console.log("get token inicial end")
+        this.componentLoading = false
+      })
+  },
+
+    methods: {
+      ...mapMutations('appToken', [ "setAppToken" ] ),
+      ...mapMutations('usuario', [ "userIsAdmin", "setUsuarioToken"]),
+    
+      cargarLocalStorageData() {
+        if (localStorage.getItem("admin")==="true") this.userIsAdmin(true)
+        else this.userIsAdmin(false)
+
+        if (localStorage.getItem("userToken")) this.setUsuarioToken(localStorage.getItem("userToken"))
+
+        if (this.admin && this.token ) this.$router.push({ name: "MuniAdmin" })
+        if (!this.admin && this.token ) this.$router.push({ name: "MuniAulaVirtual" })
+      }
+    }
 }
 </script>
+
+<style scoped>
+.app-loader-content {
+  top: 250px;
+  position:relative;
+  width: 100%;
+}
+.loader {
+  left: 50%;
+  transform:translate(-50%, -50%);
+}
+</style>
