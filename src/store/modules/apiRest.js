4bd9d8f923e7579dcc6ffd6378ad2456c757738d
AlexanderHOB <obispoa1@gmail.com>
2020-12-12 00:16:04 -0500

    Add env

--- src/store/modules/apiRest.js
+++ src/store/modules/apiRest.js
@@ -4,6 +4,7 @@ export default {
     //url:"https://aula-virtual-eltambo.teampixeland.com/public",
     url:"http://127.0.0.1:8000/api", // local
     // url:"https://test.teampixeland.com/API_ACADEMIA_MUNI/public/api", //testeo-red
+    // api: "https://api.academia-el-tambo.teampixeland.com/API_ACADEMIA_MUNI/public/api", // prod
     url_node:"https://ubigeo.teampixeland.com/"
 
   }
